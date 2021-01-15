6c8a222ec231d38139173779a66a142f3d281baa
theus520 <matheusrabelo520@gmail.com>
2021-01-14 21:31:00 -0300

    primeiro commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import React,{Component} from 'react'
+import {Platform,StyleSheet,Text,View} from 'react-native'
+import Button from './src/components/Button'
+
+export default class App extends Component{
+
+  render(){
+
+    return(
+      <View style={styles.container}>
+<View style={styles.buttons}>
+      <Button label='AC' />
+      <Button label='/' />
+      <Button label='7' />
+      <Button label='8' />
+      <Button label='9' />
+      <Button label='*' />
+      <Button label='4' />
+      <Button label='5' />
+      <Button label='6' />
+      <Button label='-' />
+      <Button label='1' />
+      <Button label='2' />
+      <Button label='4' />
+      <Button label='+' />
+      <Button label='0' />
+      <Button label='.' />
+      <Button label='=' />
+
+      </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+container: {
+flex: 1
+},
+buttons:{
+  flexDirection: 'row',
+  flexWrap: 'wrap'
+}
+
+})
